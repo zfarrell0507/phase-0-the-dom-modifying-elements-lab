fd02673e9150cbcdc789564618d1d59e4c15105b
Zachary Farrell <zfarrell0507@gmail.com>
2022-07-09 13:45:06 -0400

    commit tree 49058faf79b9b4f95e1c0adb583caeda4b1f4a78

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-// Write your code here!
+const element = document.getElementById("main");
+element.remove();
+const newHeader = document.createElement("h1");
+document.body.append(newHeader);
+const victory = document.createElement("victory");
+victory.id = "victory";
+victory.textContent = ("Zachary is the champion");
+newHeader.append(victory);
